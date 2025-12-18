c5f94461219ec1e089e995562c01739072ccb7d7
d114mv <dilanimurielv@gmail.com>
2025-12-18 10:43:15 -0400

    version_2.1.8

--- backend/bot_worker.py
+++ backend/bot_worker.py
@@ -26,7 +26,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-TOKEN = os.getenv("BOT_TOKEN", "8577123738:AAEq9ep-nunzc2ONbqyZwIs7JrNrhFrkPe0")
+TOKEN = os.getenv("BOT_TOKEN", "8577123738:AAFddC6JHqCAwLcNCJC3NPrC7Obp9x2XAI0")
 API_URL = os.getenv("API_URL", "https://info-emi-backend.onrender.com")
 
 bot = telebot.TeleBot(TOKEN, parse_mode="HTML")
